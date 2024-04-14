6d48bbdbfd86411fad59654657d8c50b812c5316
Marius Krintel <mkrin23@student.sdu.dk>
2024-04-14 12:39:25 +0200

    begin

---
+++ build/CMakeFiles/Aktiekurs.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Aktiekurs"
+  "Aktiekurs.pdb"
+  "CMakeFiles/Aktiekurs.dir/main.cpp.o"
+  "CMakeFiles/Aktiekurs.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Aktiekurs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/marius/Documents/RD2-CPP/PA2 - Aktie
+# Build directory: /home/marius/Documents/RD2-CPP/PA2 - Aktie/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
