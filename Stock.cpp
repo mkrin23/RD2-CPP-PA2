8599c5cde3e9e98340272bc6e1bcefa623cf1ae1
Marius Krintel <mkrin23@student.sdu.dk>
2024-04-21 19:58:09 +0200

    added coments to TradeInfo and Stock, but main does not work

--- Stock.cpp
+++ Stock.cpp
@@ -13,48 +13,56 @@ void Stock::setAllTradeInfo(const std::vector<TradeInfo> &tradeInfo){
 };
 
 std::vector<TradeInfo> Stock::getTrades(unsigned int interval){
+    // create return vector and dummy TradeInfo
     std::vector<TradeInfo> ret;
     TradeInfo ti;
     
+    // create all variables
     double open, high = 0, low = 0, close, volume = 0;
     bool bOpen = 1;
     int i = 0;
+    //for every value in vector
     for(auto it = mTradeInfo.begin(); it != mTradeInfo.end(); ++it){
         if(bOpen){
+            //if first value after new
             ti.setOpen((*it).getOpen());
             ti.setDateTime((*it).getDateTime());
 
             bOpen = 0;
         }
 
+        //save new close walue
         close = (*it).getClose();
 
+        //check if high and low is better
         high = ((*it).getHigh() > high) ? (*it).getHigh() : high;
         low = ((*it).getLow() < low) ? (*it).getLow() : low;
 
+        //sum all volumes
         volume += (*it).getVolume();
 
-        if( (interval == TRADE_INTERVAL_MINUTE && ((*it).getDateTime().getMinute() > ti.getDateTime().getMinute() ||
+        if( (interval == TRADE_INTERVAL_MINUTE && ((*it).getDateTime().getMinute() > ti.getDateTime().getMinute() ||// if set to pr minut
                                                    (*it).getDateTime().getHour() > ti.getDateTime().getHour() ||
                                                    (*it).getDateTime().getDay() > ti.getDateTime().getDay() ||
                                                    (*it).getDateTime().getMonth() > ti.getDateTime().getMonth() ||
                                                    (*it).getDateTime().getYear() > ti.getDateTime().getYear())) ||
-            (interval == TRADE_INTERVAL_HOUR && ((*it).getDateTime().getHour() > ti.getDateTime().getHour() ||
+            (interval == TRADE_INTERVAL_HOUR && ((*it).getDateTime().getHour() > ti.getDateTime().getHour() || // if set to per hr
                                                  (*it).getDateTime().getDay() > ti.getDateTime().getDay() ||
                                                  (*it).getDateTime().getMonth() > ti.getDateTime().getMonth() ||
                                                  (*it).getDateTime().getYear() > ti.getDateTime().getYear())) ||
-            (interval == TRADE_INTERVAL_2HOUR && ((*it).getDateTime().getHour() > (ti.getDateTime().getHour()+1) ||
+            (interval == TRADE_INTERVAL_2HOUR && ((*it).getDateTime().getHour() > (ti.getDateTime().getHour()+1) || // if set to pr 2 hr
                                                  (*it).getDateTime().getDay() > ti.getDateTime().getDay() ||
                                                  (*it).getDateTime().getMonth() > ti.getDateTime().getMonth() ||
                                                  (*it).getDateTime().getYear() > ti.getDateTime().getYear())) ||
-            (interval == TRADE_INTERVAL_4HOUR && ((*it).getDateTime().getHour() > (ti.getDateTime().getHour()+3) ||
+            (interval == TRADE_INTERVAL_4HOUR && ((*it).getDateTime().getHour() > (ti.getDateTime().getHour()+3) || // if set to pr 4 hr
                                                  (*it).getDateTime().getDay() > ti.getDateTime().getDay() ||
                                                  (*it).getDateTime().getMonth() > ti.getDateTime().getMonth() ||
                                                  (*it).getDateTime().getYear() > ti.getDateTime().getYear())) ||
-            (interval == TRADE_INTERVAL_DAY && ((*it).getDateTime().getDay() > ti.getDateTime().getDay() ||
+            (interval == TRADE_INTERVAL_DAY && ((*it).getDateTime().getDay() > ti.getDateTime().getDay() || // if set to per day
                                                 (*it).getDateTime().getMonth() > ti.getDateTime().getMonth() ||
                                                 (*it).getDateTime().getYear() > ti.getDateTime().getYear()))
            ){
+            //insert saved values in element in return vector
             ti.setClose((*it).getClose());
             ti.setHigh(high);
             high = 0;
--- TradeLoader.cpp
+++ TradeLoader.cpp
@@ -7,11 +7,12 @@ TradeLoader::TradeLoader(){}
 
 void TradeLoader::load(const std::string& filename){
     std::string data;
-
+    // open file
     std::ifstream dataStream(filename);
-    std::array<std::string,6> row;
+    // read every line
     std::getline(dataStream,data);
     while (std::getline(dataStream,data)){
+        //split line into cells and insert into member vector
         mData.push_back(split(data,','));
     }
 }
@@ -22,13 +23,18 @@ std::array<std::string, 6> TradeLoader::split(const std::string& line, char deli
     std::string word = "";
 
     for(int c = 0; c < line.length(); ++c){
+        //check each character
         if (line[c] == delim){
+            // check if is delim
+            //save word and start anew
             data[i++] = word;
             word = "";
             continue;
         }
+        //else add character to word
         word = word+line[c];
     }
+    //save last word and return
     data[i] = word;
     return data;
 }
@@ -37,6 +43,7 @@ std::vector<TradeInfo> TradeLoader::getTradeInfo(){
     std::vector<TradeInfo> trades;
 
     for (std::array<std::string,6> row : mData){
+        //create TradeInfo for each line
         trades.push_back(TradeInfo(DateTime(row[0]),
                          std::stod(row[1]),
                          std::stod(row[2]),
